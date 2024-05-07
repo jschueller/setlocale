b86414e96d64b10cb92cb3e75e86c8824dced130
Julien Schueller <schueller@phimeca.com>
2024-05-07 19:45:54 +0200

    check

--- main2.cxx
+++ main2.cxx
@@ -10,16 +10,19 @@ int main(int, const char* [])
   locale_t new_locale;
   new_locale = newlocale (LC_NUMERIC_MASK, "fr_FR.utf-8", NULL);
   std::cout << "new_locale="<<new_locale<<std::endl;
-  
-  locale_t old_locale = uselocale(new_locale);
-  std::cout << "old_locale="<<old_locale<<std::endl;
 
-  char szOrbits[] = "365,24 29,53";
-  double v = strtod (szOrbits, NULL);
-  std::cout << "v="<<v<<std::endl;
-  
-  uselocale(old_locale);
-  freelocale(new_locale);
+  if (new_locale!=0)
+  {
+    locale_t old_locale = uselocale(new_locale);
+    std::cout << "old_locale="<<old_locale<<std::endl;
+
+    char szOrbits[] = "365,24 29,53";
+    double v = strtod (szOrbits, NULL);
+    std::cout << "v="<<v<<std::endl;
+    
+    uselocale(old_locale);
+    freelocale(new_locale);
+  }
 
   return 0;
 }
